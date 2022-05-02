afc554988f8c839da5f2a3f60e68c8d706744781
janaestewart <janae.stewart94@gmail.com>
2022-05-02 10:34:51 -0400

    testing the 18% radio button

--- app/src/androidTest/java/com/example/tiptime/CalculatorTests.kt
+++ app/src/androidTest/java/com/example/tiptime/CalculatorTests.kt
@@ -7,6 +7,7 @@ import androidx.test.espresso.action.ViewActions
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isChecked
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import org.hamcrest.Matchers.containsString
 import org.junit.Rule
@@ -31,10 +32,26 @@ class CalculatorTests {
     @Test
     fun calculate_20_percent_tip(){
 
+        //find a UI component to interact with, using the onView(). The function takes a ViewMatcher
+        //object parameter A ViewMatcher is essentially a UI component that matches a particular
+        // criteria, which in this case is a component that has the ID
+
+        //basically this test grabs the cost of service input, writes in 50.00 gets rid of the keyboard
+        //grabs the calculate button and clicks it
+        //and tests to make sure the
         onView(withId(R.id.cost_of_service_edit_text)).perform(typeText("50.00")).perform(ViewActions.closeSoftKeyboard())
 
         onView(withId(R.id.calculate_button)).perform(click())
 
         onView(withId(R.id.tip_result)).check(matches(withText(containsString("$10.00"))))
+    }
+
+    fun calculate_18_percent_tip(){
+        onView(withId(R.id.cost_of_service_edit_text)).perform(typeText("50.00")).perform(ViewActions.closeSoftKeyboard())
+        onView(withId(R.id.option_eighteen_percent)).perform(click())
+        onView(withId(R.id.calculate_button))
+            .perform(click())
+        onView(withId(R.id.tip_result))
+            .check(matches(withText(containsString("$9.00"))))
     }
 }
